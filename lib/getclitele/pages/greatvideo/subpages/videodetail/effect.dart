d6c2b3190d1c9baa6dd2977b5ea7ce5c1846ae9f
陈庆 <chenqing873@1caifu.com>
2020-04-15 18:32:45 +0800

    视频播放优化

--- lib/getclitele/pages/greatvideo/subpages/videodetail/effect.dart
+++ lib/getclitele/pages/greatvideo/subpages/videodetail/effect.dart
@@ -16,15 +16,15 @@ void _onAction(Action action, Context<VideoDetailState> ctx) {
 }
 void _onInit(Action action, Context<VideoDetailState> ctx) {
     ctx.state.playerController = VideoPlayerController.network(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
-    ctx.state.initializeVideoPlayerFuture = ctx.state.playerController.initialize();
-//    设置循环播放
-    ctx.state.playerController.setLooping(true);
+        'http://1caifu-file-public.oss-cn-hangzhou.aliyuncs.com/video/business-institute/'
+            '%E5%BE%AE%E8%AF%BE%E4%BA%94%EF%BC%9A%E9%98%B3%E5%85%89%E7%A7%81%E5%8B%9F%E7%BC%98%E4%BD%95%E9%98%B3%E5%85%89-6bc06548959046dfb7cd77e2e7240202.mp4');
     ctx.state.chewieController = ChewieController(
       videoPlayerController: ctx.state.playerController,
-      aspectRatio: 1,
+      aspectRatio: 16/9,
       autoPlay: true,
-      looping: true
+      looping: true,
+      allowFullScreen: true, //是否允许全屏
+//      customControls:
     );
 }
 void _dispose(Action action,Context<VideoDetailState> ctx){
--- lib/getclitele/pages/greatvideo/subpages/videodetail/view.dart
+++ lib/getclitele/pages/greatvideo/subpages/videodetail/view.dart
@@ -14,25 +14,28 @@ Widget buildView(VideoDetailState state, Dispatch dispatch, ViewService viewServ
         new NavBar(
           titleStr: state.navtitle,
         ),
-        FutureBuilder(
-          future: state.initializeVideoPlayerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              // If the VideoPlayerController has finished initialization, use
-              // the data it provides to limit the aspect ratio of the video.
-              return AspectRatio(
-                aspectRatio: state.playerController.value.aspectRatio,
-                // Use the VideoPlayer widget to display the video.
-                child: VideoPlayer(state.playerController),
-              );
-            } else {
-              // If the VideoPlayerController is still initializing, show a
-              // loading spinner.
-              return Center(child: CircularProgressIndicator());
-            }
-          },
+        Chewie(
+          controller: state.chewieController,
         ),
-        new RaisedButton(
+//        FutureBuilder(
+//          future: state.initializeVideoPlayerFuture,
+//          builder: (context, snapshot) {
+//            if (snapshot.connectionState == ConnectionState.done) {
+//              // If the VideoPlayerController has finished initialization, use
+//              // the data it provides to limit the aspect ratio of the video.
+//              return AspectRatio(
+//                aspectRatio: state.playerController.value.aspectRatio,
+//                // Use the VideoPlayer widget to display the video.
+//                child: VideoPlayer(state.playerController),
+//              );
+//            } else {
+//              // If the VideoPlayerController is still initializing, show a
+//              // loading spinner.
+//              return Center(child: CircularProgressIndicator());
+//            }
+//          },
+//        ),
+        new FloatingActionButton(
             onPressed: (){
               state.playerController.value.isPlaying
                   ? state.playerController.pause()
